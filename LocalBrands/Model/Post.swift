3300678d9e537077b865cd52604687543c212b1c
Lujain <lujain_mohammed@outlook.sa>
2023-06-02 19:47:59 +0300

    1

---
+++ LocalBrands/Model/Post.swift
@@ -0,0 +1,20 @@
+//
+//  Post.swift
+//  LocalBrands
+//
+//  Created by lujain mohammed on 13/11/1444 AH.
+//
+
+import SwiftUI
+
+struct Post: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Post_Previews: PreviewProvider {
+    static var previews: some View {
+        Post()
+    }
+}
